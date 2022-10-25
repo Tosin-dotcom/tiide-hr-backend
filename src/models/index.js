9e9b7f340d6488e8f997c245f24a29eacccad9f1
Tosin <tosiniyiola00@gmail.com>
2022-10-25 09:41:09 +0100

    commit

--- src/models/index.js
+++ src/models/index.js
@@ -1,22 +1,22 @@
 const Sequelize = require('sequelize');
-const { sequelize } = require('../config/config');
+// const { sequelize } = require('../config/config');
 const logger = require('../config/logger');
 
-const sequelizeInstance = new Sequelize(sequelize.url);
+// const sequelizeInstance = new Sequelize(sequelize.url);
+const sequelizeInstance = new Sequelize(process.env.JAWSDB_URL);
 const db = {};
 
-/*
-const sequelizeInstance = new Sequelize(sequelize.database, sequelize.user, sequelize.password, {
-  host: sequelize.host,
-  dialect: sequelize.dialect,
-  pool: {
-    min: 0,
-    max: 100,
-    acquire: 5000,
-    Idle: 1000
-  },
-});
-*/
+// const sequelizeInstance = new Sequelize(sequelize.database, sequelize.user, sequelize.password, {
+//   host: sequelize.host,
+//   dialect: sequelize.dialect,
+//   pool: {
+//     min: 0,
+//     max: 100,
+//     acquire: 5000,
+//     Idle: 1000,
+//   },
+// });
+
 sequelizeInstance
   .authenticate()
   .then(() => logger.info('DB connected'))
