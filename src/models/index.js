bca7fffb35f76ce5b415716ab2b99691c672568d
Eduvie Owen <eduvieouu@gmail.com>
2022-10-25 16:12:48 +0100

    testing deployment

--- src/models/index.js
+++ src/models/index.js
@@ -3,19 +3,20 @@ const { sequelize } = require('../config/config');
 const logger = require('../config/logger');
 
 const sequelizeInstance = new Sequelize(sequelize.url);
-
 const db = {};
-// const sequelizeInstance = new Sequelize(sequelize.database, sequelize.user, sequelize.password, {
-//   host: sequelize.host,
-//   dialect: sequelize.dialect,
-//   pool: {
-//     min: 0,
-//     max: 100,
-//     acquire: 5000,
-//     Idle: 1000,
-//   },
-// });
 
+/*
+const sequelizeInstance = new Sequelize(sequelize.database, sequelize.user, sequelize.password, {
+  host: sequelize.host,
+  dialect: sequelize.dialect,
+  pool: {
+    min: 0,
+    max: 100,
+    acquire: 5000,
+    Idle: 1000
+  },
+});
+*/
 sequelizeInstance
   .authenticate()
   .then(() => logger.info('DB connected'))
@@ -28,6 +29,8 @@ db.Sequelize = Sequelize;
 
 db.users = require('./user.model')(sequelizeInstance, Sequelize);
 db.tokens = require('./token.model')(sequelizeInstance, Sequelize);
+db.staffs = require('./staff.model')(sequelizeInstance, Sequelize);
+db.leaves = require('./leave.model')(sequelizeInstance, Sequelize);
 
 // relationships for models
 
@@ -36,6 +39,16 @@ db.tokens = require('./token.model')(sequelizeInstance, Sequelize);
 //= ==============================
 // db.User.hasMany(db.Role);
 // db.Role.belongsTo(db.User);
+
+// hasOne
+db.users.hasOne(db.staffs);
+db.staffs.belongsTo(db.users);
+// hasMany
+db.staffs.hasMany(db.leaves);
+db.leaves.belongsTo(db.staffs);
+
+// db.staffs.belongsToMany(db.leaves, { through: 'staffs_leaves' });
+// db.leaves.belongsToMany(db.staffs, { through: 'staffs_leaves' });
 
 module.exports = {
   db,
